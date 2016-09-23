c17d20405abe3e221eba524a90040a205e299ed8
teamcobby <support@cobbysoft.com>
2016-09-23 16:23:07 -0700

    Revised seeds.rb to seed brandings and plans.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160903051413) do
+ActiveRecord::Schema.define(version: 20160923103528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20160903051413) do
   create_table "plans", force: :cascade do |t|
     t.string   "stripe_plan_id"
     t.string   "name"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  create_table "settings", force: :cascade do |t|
+    t.integer  "account_id"
+    t.string   "google_api_key"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
   end
